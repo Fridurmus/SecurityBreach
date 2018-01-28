d4378006b05b531bb0c4ce92b6b5d8caf4ddc9b2
Luis <luisriverapolanco@gmail.com>
2018-01-27 23:24:23 -0500

    Made door and terminal

---
+++ Security Breach/Assets/Script/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public Terminal[] NeededTerminals;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CheckItem())
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    bool CheckItem()
+    {
+        foreach (Terminal item in NeededTerminals)
+        {
+            if (item.triggered)
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Security Breach/Assets/Script/Terminal.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Terminal : MonoBehaviour
+{
+
+    public bool triggerable = false;
+    public bool triggered = false;
+    Light ourLight;
+
+    private void Start()
+    {
+        ourLight = gameObject.GetComponentInChildren<Light>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ourLight.color = Color.blue;
+            //play sound effect
+            triggerable = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ourLight.color = Color.gray;
+            triggerable = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerable && Input.GetButtonDown("use"))
+        {
+            triggered = true;
+            //play sound effect
+        }
+
+        if (triggered)
+        {
+            ourLight.color = Color.green;
+        }
+    }
+}
