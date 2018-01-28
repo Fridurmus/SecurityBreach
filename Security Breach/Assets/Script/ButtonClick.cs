e217a21a4b59a3b7d5ab191e17f3372e0fed033c
Sean Davis <Fridurmus@gmail.com>
2018-01-28 05:28:38 -0500

    Main Menu button SFX implemented
    Main menu button SFX and handlers implemented

---
+++ Security Breach/Assets/Script/ButtonClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    public Button clickedButton;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        clickedButton.onClick.AddListener(playOnClick);
+    }
+
+    public void playOnClick()
+    {
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+}
---
+++ Security Breach/Assets/Script/ButtonHover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler {
+    public AudioSource audioSource;
+	
+	public void OnPointerEnter (PointerEventData ped) {
+        audioSource.PlayOneShot(audioSource.clip);
+	}
+}
