f3a53955e36d0fd3bb16e23100bc54df0ed51e3a
Luis <luisriverapolanco@gmail.com>
2018-01-27 20:30:25 -0500

    added some things

---
+++ Security Breach/Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour
+{
+    public GameObject creditsBox;
+
+    private bool isScrolling;
+
+    public float scrollSpeed;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Setup();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Scroll();
+	}
+
+    void Setup()
+    {
+        isScrolling = true;
+    }
+
+    void Scroll()
+    {
+        if(isScrolling == true)
+        {
+            creditsBox.transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed);
+        }
+    }
+
+
+}
---
+++ Security Breach/Assets/Scripts/TextboxScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextboxScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TestInput();
+	}
+
+    public void TestInput()
+    {
+        if(Input.GetKeyDown("t"))
+        {
+            Debug.Log("Key was pressed");
+        }
+    }
+
+}
